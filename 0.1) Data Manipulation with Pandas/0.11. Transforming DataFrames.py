047af965dd9d1e79cc707bd5a04ea98523b1c4e4
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-09 21:07:28 +0000

    modified:   0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py
@@ -2,3 +2,20 @@
 # - Sorting and subsetting. 
 # - Creating new columns. 
 
+# Pandas is built on top of: 
+# 1. NumPy - provides multi-dimensional array objects for easy data 
+# #   manipulation that Pandas uses to store data. 
+# 2. Matplotlib - provides powerful data visualisation capabilities that
+#     Pandas takes advantage of. 
+
+# Rectangular data/tabluar data is the most common form of data storage. 
+# Rectangular data = DataFrame. 
+
+# Different cols can contain different data types. 
+# Same col only contains same data type.
+
+print(dataframename.head()) # Shows the first 5 rows of the DataFrame.
+
+print(dataframename.info()) # Shows the data types of each col and the 
+#                               number of non-missing values.
+
