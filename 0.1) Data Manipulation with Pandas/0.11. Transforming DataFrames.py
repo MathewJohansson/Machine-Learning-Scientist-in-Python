04260e543c0c0ac10160e82847dcdd944f673a2a
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-09 21:18:58 +0000

    modified:   0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.11. Transforming DataFrames.py
@@ -19,3 +19,21 @@ print(dataframename.head()) # Shows the first 5 rows of the DataFrame.
 print(dataframename.info()) # Shows the data types of each col and the 
 #                               number of non-missing values.
 
+print(dataframename.shape()) # Shows the number of rows and cols in the 
+#                                DataFrame.
+
+print(dataframename.describe()) # Shows the summary statistics of the
+#                                   DataFrame.
+
+print(dataframename.values()) # Shows the values of the DataFrame as a
+#                                 2D NumPy array.  
+
+print(dataframename.columns()) # Shows the column names of the DataFrame.
+
+print(dataframename.index()) # Shows the index of the DataFrame.
+
+
+# Pandas Philosopy:
+#   There should be one - and preferably only one - obvious way to do it.
+
+
