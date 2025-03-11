11913b3643eb321741aca717ef6c5eb6f86b5d25
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-11 11:53:34 +0000

    modified:   0.1) Data Manipulation with Pandas/0.12. Aggregating DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.12. Aggregating DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.12. Aggregating DataFrames.py
@@ -53,3 +53,21 @@ dogs["weight_kg"].cumsum()
 # .cummin()
 # .cumprod()
 
+
+# E.g., cumulative statistics for store department sales:
+
+# Sort sales_1_1 by date
+sales_1_1 = sales_1_1.sort_values('date')
+
+# Get the cumulative sum of weekly_sales, add as cum_weekly_sales col
+sales_1_1["cum_weekly_sales"] = sales_1_1.weekly_sales.cumsum()
+
+# Get the cumulative max of weekly_sales, add as cum_max_sales col
+sales_1_1["cum_max_sales"] = sales_1_1.weekly_sales.cummax()
+
+print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
+
+
+
+# Counting
+
