13a314acbf8500a66ac445581177d8ea5761acb7
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-12 15:46:07 +0000

    modified:   0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
@@ -6,5 +6,49 @@
 
 # Explicit Indexes
 
-# Delete when starting:
-# https://campus.datacamp.com/courses/data-manipulation-with-pandas/slicing-and-indexing-dataframes?ex=1
+# Setting a column as the index
+dogs_ind = dogs.set_index("name")
+print(dogs_ind)
+
+# To undo 
+dogs_ind.reset_index()
+
+# Remove/drop an index
+dogs_ind.reset_index(drop=True)
+
+# Indexes make subsetting simpler. Instead of: 
+dogs[dogs["name"].isin(["Bella", "Stella"])]
+# we can use: 
+dogs_ind.loc[["Bella", "Stella"]]
+
+
+# Index values don't need to be unique
+dogs_ind2 = dogs.set_index("breed")
+
+# then subset on Labrador for those rows
+dogs_ind2.loc["Labrador"]
+
+
+# Multi-level indexes (hierarchical indexes)
+dogs_ind3 = dogs.set_index(["breed", "color"])
+# this puts breed as the left-most index col, and color to the right of that.
+
+# Subset the outer level with a list 
+dogs_ind3.loc[["Labrador", "Chihuahua"]]
+
+# Subsetting on inner levels 
+dogs_ind3.loc[[("Labrador", "Brown"), ("Chihuahua", "Tan")]]
+
+
+# Sort by index values
+dogs_ind3.sort_index()
+
+# Controlling sort_index 
+dogs_ind3.sort_index(level=["color", "breed"], ascending=[True, False])
+
+
+# Problems with indexes:
+# - Index values are just data. 
+# - Indexes violate "tidy data" principles. 
+# - Two syntaxes need to be learned. 
+
