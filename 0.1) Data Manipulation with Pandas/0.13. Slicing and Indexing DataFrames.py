aa8fb3ff5553b30bc1b2cc6b8d04c3f982f7d787
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-14 06:00:19 +0000

    modified:   0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
@@ -95,3 +95,27 @@ dogs.loc["2014":"2016"]
 # Slicing using iloc method
 dog.iloc[2:5, 1:4]
 
+# -----------------------------
+
+# E.g., temperatures of cities
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
+
+# Subset rows from Pakistan to Russia
+print(temperatures_srt.loc["Pakistan":"Russia"])
+
+# Try to subset rows from Lahore to Moscow
+print(temperatures_srt.loc["Lahore":"Moscow"])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[("Pakistan", "Lahore"):("Russia", "Moscow")])
+
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[("India", "Hyderabad"):("Iraq", "Baghdad")])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:, "date":"avg_temp_c"])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[("India", "Hyderabad"):("Iraq", "Baghdad"), "date":"avg_temp_c"])
+
