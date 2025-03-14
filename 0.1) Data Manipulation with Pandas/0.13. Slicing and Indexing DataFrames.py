4110ce5ed7512dd8aaccf601937018bdaad5412f
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-14 06:08:23 +0000

    modified:   0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py

--- 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
+++ 0.1) Data Manipulation with Pandas/0.13. Slicing and Indexing DataFrames.py
@@ -107,6 +107,7 @@ print(temperatures_srt.loc["Pakistan":"Russia"])
 # Try to subset rows from Lahore to Moscow
 print(temperatures_srt.loc["Lahore":"Moscow"])
 
+
 # Subset rows from Pakistan, Lahore to Russia, Moscow
 print(temperatures_srt.loc[("Pakistan", "Lahore"):("Russia", "Moscow")])
 
@@ -118,4 +119,18 @@ print(temperatures_srt.loc[:, "date":"avg_temp_c"])
 
 # Subset in both directions at once
 print(temperatures_srt.loc[("India", "Hyderabad"):("Iraq", "Baghdad"), "date":"avg_temp_c"])
+
+
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+temperatures_bool = temperatures[(temperatures["date"] >= "2010-01-01") & (temperatures["date"] <= "2011-12-31")]
+print(temperatures_bool)
+
+# Set date as the index and sort the index
+temperatures_ind = temperatures.set_index("date").sort_index()
+
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc["2010":"2011"])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc["2010-08-01":"2011-02-28"])
 
