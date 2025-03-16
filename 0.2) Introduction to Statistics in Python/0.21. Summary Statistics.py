90a1ea3153ddaa95aed3f54b4bacdf915c253037
MathewJohansson <mathew.johansson1@outlook.com>
2025-03-16 19:32:17 +0000

    modified:   0.2) Introduction to Statistics in Python/0.21. Summary Statistics.py

---
+++ 0.2) Introduction to Statistics in Python/0.21. Summary Statistics.py
@@ -0,0 +1,15 @@
+# Field of statistics - the practice and study of collecting and 
+#                       analysing data. 
+
+# A summary statistic - a fact about or summary of some data. 
+
+
+# Types of statistics:
+# 1. Descriptive statistics - describe and summarise data. 
+#    E.g., 50% of friends drive to work. 
+#          25% take the bus. 
+
+# 2. Inferential statistics - use a sample of data to make inferences
+#                             about a larger population. 
+#    E.g., what percentage of people drive to work, based on our data?
+
